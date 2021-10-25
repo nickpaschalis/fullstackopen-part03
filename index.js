21af8e516495b86b2386a25b26d5fe360235d2af
Nick Paschalis <lesrevenants1967@gmail.com>
2021-10-25 09:57:35 +0300

    3.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 let persons = [
   { 
     "id": 1,
@@ -48,6 +49,18 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id);
   response.status(204).end();
 });
+
+app.post('/api/persons', (request, response) => {
+  const id = Math.floor(Math.random() * 1000000000);
+  const body = request.body;
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person);
+  response.json(person);
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
